59b3c3930006e923e5f81fd51826657f929c8e86
Jan Kapko <kamo.bavmesa@gmail.com>
2025-12-25 20:39:19 +0100

    automatic help messages and special values for help and usage generation

--- src/main.zig
+++ src/main.zig
@@ -2,35 +2,38 @@ const std = @import("std");
 const zgra = @import("zgra");
 
 const Args = struct {
-    _help: bool,
-    _version: bool,
-    _xd: bool,
-    int: i128,
-    uint: u64,
-    flt: f16,
-    msg: [:0]const u8,
-    msg2: [:0]u8,
-    msg3: []const u8,
-    msg4: []u8,
+    __program: []const u8 = "zgra_showcase",
+    __usage: []const u8 = "[-options] arguments",
+    __help: []const u8 = "showcase of the zgra zig command line argument parsing library",
+    __version: []const u8 = "v0.0.1",
+    int: i128 = 0,
+    uint: u64 = 0,
+    flt: f16 = 0,
+    msg: [:0]const u8 = "",
+    msg2: [:0]u8 = @constCast(""),
+    msg3: []const u8 = "",
+    msg4: []u8 = "",
     opt: ?bool = null,
 };
-const Parser = zgra.MakeParser(Args);
+
+const Parser = zgra.MakeParser(
+    Args,
+    zgra.ZgraHelp{ .items = &.{
+        .{ .name = "int", .help = "parse a signed integer" },
+        .{ .name = "uint", .help = "parse an unsigned integer" },
+        .{ .name = "flt", .help = "parse a float" },
+        .{ .name = "msg", .help = "parse a string" },
+    } },
+);
 
 pub fn main() !void {
+    var buff: [1024]u8 = undefined;
+    var w = std.fs.File.stderr().writer(&buff);
     var it = std.process.ArgIterator.init();
-    var parser = Parser{ .template = .{
-        ._help = false,
-        ._version = false,
-        ._xd = false,
-        .int = 0,
-        .uint = 0,
-        .flt = 0.0,
-        .msg = "",
-        .msg2 = @constCast(""),
-        .msg3 = "",
-        .msg4 = "",
-    } };
-    const res = try parser.parse(&it);
+    var parser = Parser{
+        .template = .{},
+    };
+    const res = try parser.parse(&it, &w.interface);
 
     std.debug.print("{any}\n", .{res});
 }
--- src/root.zig
+++ src/root.zig
@@ -9,6 +9,7 @@ pub const ZgraError = error{
     InvalidArgumentOrder,
     Overflow,
     InvalidCharacter,
+    WriteFailed,
 };
 
 pub const Arg = struct {
@@ -22,9 +23,18 @@ pub const Arg = struct {
     },
     short: bool,
     optional: bool,
+    skip: bool,
+    help: []u8 = "",
 };
 
-pub const Zgra = struct {
+const ZgraMeta = struct {
+    help: []const u8 = "",
+    usage: []const u8 = "",
+    program: []const u8 = "",
+    version: []const u8 = "",
+};
+
+const Zgra = struct {
     currentArg: ?Arg = undefined,
     state: enum {
         arg,
@@ -32,38 +42,73 @@ pub const Zgra = struct {
     } = .arg,
 };
 
-pub fn MakeParser(comptime Template: type) type {
+pub const ZgraHelp = struct {
+    items: []const ZgraArg,
+};
+
+pub const ZgraArg = struct {
+    name: []const u8,
+    help: []const u8,
+};
+
+pub fn MakeParser(comptime Template: type, helpInfo: ZgraHelp) type {
     const info = @typeInfo(Template).@"struct";
     const fields = comptime make_fields: {
         var fields: [info.fields.len]Arg = undefined;
         var sfields: [info.fields.len]StructField = undefined;
-        for (info.fields, 0..) |f, i| {
+        var meta: ZgraMeta = .{};
+        for (
+            info.fields,
+            0..,
+        ) |f, i| {
             const optionalField = switch (@typeInfo(f.type)) {
                 .optional => |x| .{ x.child, true },
                 else => .{ f.type, false },
             };
-            fields[i] = Arg{
-                .name = f.name,
-                .type = switch (optionalField[0]) {
-                    bool => .bool,
-                    i8, i16, i32, i64, i128, isize => .int,
-                    u8, u16, u32, u64, u128, usize => .uint,
-                    []const u8, []u8, [:0]const u8, [:0]u8 => .str,
-                    f16, f32, f64, f80, f128 => .float,
-                    else => @compileError("unsupported field type"),
-                },
-                .short = f.name[0] == '_',
-                .optional = optionalField[1],
-            };
+            const help = std.mem.eql(u8, "__help", f.name);
+            const usage = std.mem.eql(u8, "__usage", f.name);
+            const version = std.mem.eql(u8, "__version", f.name);
+            const program = std.mem.eql(u8, "__program", f.name);
+            const skip = help or version or usage or program;
+            // meta args
+            if (help) {
+                meta.help = f.defaultValue().?;
+            } else if (version) {
+                meta.version = f.defaultValue().?;
+            } else if (usage) {
+                meta.usage = f.defaultValue().?;
+            } else if (program) {
+                meta.program = f.defaultValue().?;
+            } else if (f.name.len > 2 and f.name[0] == '_' and f.name == '_') {
+                @compileError("not a supported meta value: " ++ f.name);
+            }
+
+            // regular args
+            fields[i] = Arg{ .name = f.name, .type = switch (optionalField[0]) {
+                bool => .bool,
+                i8, i16, i32, i64, i128, isize => .int,
+                u8, u16, u32, u64, u128, usize => .uint,
+                []const u8, []u8, [:0]const u8, [:0]u8 => .str,
+                f16, f32, f64, f80, f128 => .float,
+                else => @compileError("unsupported field type"),
+            }, .short = f.name[0] == '_', .optional = optionalField[1], .skip = skip, .help = getinfo: {
+                for (helpInfo.items) |ai| {
+                    if (std.mem.eql(u8, ai.name, f.name)) {
+                        break :getinfo @constCast(ai.help);
+                    }
+                }
+                break :getinfo "";
+            } };
             sfields[i] = f;
         }
-        break :make_fields .{ fields, sfields };
+        break :make_fields .{ fields, sfields, meta };
     };
     return struct {
         parser: Zgra = .{},
+        meta: ZgraMeta = fields[2],
         template: Template,
 
-        pub fn parse(self: *@This(), it: *std.process.ArgIterator) ZgraError!Template {
+        pub fn parse(self: *@This(), it: *std.process.ArgIterator, w: *std.Io.Writer) ZgraError!Template {
             self.parser.state = .arg;
             // skip program name
             _ = it.next();
@@ -75,6 +120,12 @@ pub fn MakeParser(comptime Template: type) type {
                             "--",
                             arg[0..2],
                         )) {
+                            if (std.mem.eql(u8, "--help", arg)) {
+                                try self.help(w);
+                            }
+                            if (std.mem.eql(u8, "--version", arg)) {
+                                try self.version(w);
+                            }
                             inline for (fields[0], fields[1]) |arg_, sf| {
                                 if (std.mem.eql(u8, arg_.name, arg[2..])) {
                                     switch (arg_.type) {
@@ -90,6 +141,12 @@ pub fn MakeParser(comptime Template: type) type {
                         } else if (arg[0] == '-') {
                             const slice = arg[1..];
                             for (slice, 0..) |b, i| {
+                                if (b == 'h') {
+                                    try self.help(w);
+                                }
+                                if (b == 'V') {
+                                    try self.version(w);
+                                }
                                 inline for (fields[0], fields[1]) |arg_, sf| {
                                     if (arg_.short and arg_.name[1] == b) {
                                         if (arg_.type != .bool and i < slice.len - 1) {
@@ -146,6 +203,31 @@ pub fn MakeParser(comptime Template: type) type {
                 }
             }
             return self.template;
+        }
+
+        fn help(self: *@This(), w: *std.Io.Writer) !noreturn {
+            try w.print("Program:\n\t{s} {s}\nAbout:\n\t{s}\nUsage:\n\t{s} {s}\n", .{ self.meta.program, self.meta.version, self.meta.help, self.meta.program, self.meta.usage });
+            try w.print("Arguments:\n", .{});
+            try w.print("\t-h, --help\t\tPrint this help message and exit.\n", .{});
+            try w.print("\t-v, --version\t\tPrint version information and exit.\n", .{});
+            inline for (fields[0]) |arg| {
+                if (arg.skip) {
+                    continue;
+                } else if (arg.short) {
+                    try w.print("\t-{c}, --{s}", .{ arg.name[1], arg.name[1..] });
+                } else {
+                    try w.print("\t--{s}", .{arg.name});
+                }
+                try w.print("\t\t\t{s}\n", .{arg.help});
+            }
+            try w.flush();
+            std.process.exit(0);
+        }
+
+        fn version(self: *@This(), w: *std.Io.Writer) !noreturn {
+            try w.print("{s}\n", .{self.meta.version});
+            try w.flush();
+            std.process.exit(0);
         }
     };
 }
